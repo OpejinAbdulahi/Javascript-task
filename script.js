4aaa7e38583af8ffd0232bfd3d2fed52fbdbd0c1
Opejin Abdulahi <opejinabdulahi.ay@gmail.com>
2020-03-26 14:10:42 +0100

    new js

---
+++ script.js
@@ -0,0 +1,22 @@
+//name
+const name= "Opejin Abdulahi"
+
+
+//courses
+const courses= ["HTLM", "CSS", "JAVASCRIPT"]
+
+
+//display name
+console.log(name)
+
+
+//display courses
+console.log(courses)
+
+
+  // find and display all odd numbers from 1-200 (inclusive)
+  for (let oddNumber = 1; oddNumber <=200; oddNumber++) {
+      if (oddNumber % 2 !== 0)
+          console.log(oddNumber);
+      }
+
